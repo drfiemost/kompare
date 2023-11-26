50a8dda76cdb83a2fbf45b0afa32c6c616e6e7f7
Leandro Nini <drfiemost@users.noreply.github.com>
2023-11-26 18:36:45 +0100

    Remove use of obsolete QStyleOption*V

--- komparepart/komparelistview.cpp
+++ komparepart/komparelistview.cpp
@@ -522,7 +522,7 @@ KompareListViewItemDelegate::~KompareListViewItemDelegate()
 void KompareListViewItemDelegate::paint( QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index ) const
 {
 	int column = index.column();
-	QStyleOptionViewItemV4 changedOption = option;
+    QStyleOptionViewItem changedOption = option;
 	if( column == COL_LINE_NO )
 		changedOption.displayAlignment = Qt::AlignRight;
 	KompareListViewItem* item = static_cast<KompareListViewItem*>( static_cast<KompareListView*>( parent() )->itemFromIndex( index ) );
@@ -616,7 +616,7 @@ void KompareListViewItem::paintCell( QPainter* p, const QStyleOptionViewItem& op
 	// We have to paint the item which shines through or we'll end up with glitches.
 	KompareListViewItem* nextItem = (KompareListViewItem*)kompareListView()->itemBelow(this);
 	if( nextItem ) {
-		QStyleOptionViewItemV4 changedOption = option;
+        QStyleOptionViewItem changedOption = option;
 		changedOption.rect.translate( 0, height() );
 		nextItem->paintCell( p, changedOption, column );
 	}
